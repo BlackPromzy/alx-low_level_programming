cfed96819eb386a3fac84365488cb6c87271702b
BlackPromzy <edungpromise1@gmail.com>
2022-10-14 21:13:40 -0700

    wembley

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,45 +2,32 @@
 
 /**
   * main - Prints 3 combination of numbers
-  *
   * Return: Always (Success)
   */
 int main(void)
 {
-	int c, i, k, j;
+	int  i, j;
 
-	for (c = 48; c <= 57; c++)
+	for (c = 0; c <= 100; c++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = 0; i < 100; i++)
 		{
-			for (k = 48; k <= 57; k++)
+			if (i > j)
 			{
-				for (j = 48; j <= 57; j++)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
+
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
