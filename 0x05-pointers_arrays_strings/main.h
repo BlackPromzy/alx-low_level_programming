0df757f2e69a5ae6bc10e9b623ea906397405b49
BlackPromzy <edungpromise1@gmail.com>
2022-10-24 19:20:06 -0700

    task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
+#define MAIN_
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,6 +7,10 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s)#
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 endif
