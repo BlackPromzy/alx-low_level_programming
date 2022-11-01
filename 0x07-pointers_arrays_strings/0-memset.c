db3a22d4e83611690b77549df2b9b75efe088eff
BlackPromzy <edungpromise1@gmail.com>
2022-10-31 19:26:58 -0700

    wembley

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * _memset - Fills memory with a constant byte n amount of times.
+ * _memset - fills memory with a constant byte n amount of times
  *
- * @s: Pointer that has the address of the memory to fill.
- * @b: Constant byte that will be written into the momory.
- * @n: Amount of times the byte will be written into the memory.
+ * @s: pointer that has the address of the memory to fill
+ * @b: constant byte that will be written into the momory
+ * @n: amount of times the byte will be written into the memory
  *
- * Return: Address of the memory where thebyte was written.
+ * Return: address of the memory where thebyte was written
  */
 char *_memset(char *s, char b, unsigned int n)
 {
