dc043efe96fec47b13a55a1525dd81b7d41dddf3
BlackPromzy <edungpromise1@gmail.com>
2022-11-08 07:40:43 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha  - Function that checks
+ * @c: Check of data
+ * Desc: print_alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c > 65  && c < 90)
+	{
+		return (1);
+	}
+	else if (c > 97 && c < 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
