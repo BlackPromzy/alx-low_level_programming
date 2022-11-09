d741f6c52dc53659aa802b71cd6ce6876313546e
BlackPromzy <edungpromise1@gmail.com>
2022-11-08 21:36:44 -0800

    tade 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,38 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main -  print name
- *@argc: int.
- *@argv: char.
- * Return: Always 0.
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
-	int j;
-	int k;
-	int mult = 0;
+	int n, ex;
 
+	ex = 0;
 	if (argc != 3)
 	{
-		printf("Error\n");
-		return (0);
+		printf("%s\n", "Error");
+		ex = 1;
 	}
-	else if (argc == 3)
+	else
 	{
-		for (i = 0 ; i < argc ; i++)
-		{
-			if (i == 1)
-			{
-				j = atoi(argv[i]);
-			}
-			else if (i == 2)
-			{
-				k = atoi(argv[i]);
-			}
-		}
-		mult = j * k;
-		printf("%d\n", mult);
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", n);
 	}
-	return (0)
+	return (ex);
 }
