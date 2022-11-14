80c3b6d1c30f92eb10bd18bf7b2a85e53338c800
BlackPromzy <edungpromise1@gmail.com>
2022-11-13 23:49:02 -0800

    tap 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,10 +3,9 @@
 /**
 * _strstr - Entry point
 * Desc: Entry
-*@haystack: char array
-*@needle: char array (password)
-*
-* Return: array
+*@haystack: pointer
+*@needle: pointer
+* Return: Always 0 (success)
 */
 char *_strstr(char *haystack, char *needle)
 {
@@ -19,11 +18,12 @@ char *_strstr(char *haystack, char *needle)
 		for (k = i, j = 0 ; needle[j] != '\0' ; j++, k++)
 		{
 			if (haystack[k] != needle[j] || haystack[k] == '\0'
-					{
+			{
 					break;
-					}
-		if (needle[j] == 0)
+			}
 		}
+		if (needle[j] == 0)
+		{
 			return (haystack + i)
 		}
 	}
