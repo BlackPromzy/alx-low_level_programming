1bc6b551a2aa6b6ccf91a2abd8f41ea80f247234
BlackPromzy <edungpromise1@gmail.com>
2022-11-14 00:32:59 -0800

    pep

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,34 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * print_diagsums - Entry point
- * @a: input
- * @size: input
+ * Desc: Entr
+ *@a: pointer
+ *@size: value
  * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n, total1 = 0, total2 = 0;
+	int i;
+	int j;
+	int dg_r = 0;
+	int dg_l = 0;
+	int k;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		total1 = total1 + a[i]
-
-			for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-				total2 = total2 + a[n];
-	printf("%d, %d\n", total1, total2);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			k = (i * size) + j;
+			if (i == j)
+			{
+				dg_r = dg_r + a[k];
+			}
+			if ((i + j) == (size - 1))
+			{
+				dg_l = dg_l + a[k];
+			}
+		}
+	}
+	printf("%d, %d\n", dg_r, dg_l);
 }
