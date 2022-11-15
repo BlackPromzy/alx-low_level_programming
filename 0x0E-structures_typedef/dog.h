50711f9ce45e6588fb396dd4d815c7b0e1b3381b
BlackPromzy <edungpromise1@gmail.com>
2022-11-14 19:04:25 -0800

    boost

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,15 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+} dog;
+
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
+
+int _putchar(char c);
 
 #endif 
